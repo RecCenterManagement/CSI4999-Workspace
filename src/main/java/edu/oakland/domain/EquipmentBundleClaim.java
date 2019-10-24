e597162c72d3a69eda691eef18bcefa1af2f094e
Noah Giles <nagiles@oakland.edu>
2019-10-24 19:16:52 -0400

    Don't serialize the many-to-one field in the relationship (avoids infinite recursion in REST fetches for EquipmentBundle)

--- src/main/java/edu/oakland/domain/EquipmentBundleClaim.java
+++ src/main/java/edu/oakland/domain/EquipmentBundleClaim.java
@@ -1,12 +1,21 @@
 package edu.oakland.domain;
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import org.hibernate.annotations.Cache;
 import org.hibernate.annotations.CacheConcurrencyStrategy;
-
-import javax.persistence.*;
-import javax.validation.constraints.*;
-
-import java.io.Serializable;
 
 /**
  * A EquipmentBundleClaim.
@@ -30,6 +39,7 @@ public class EquipmentBundleClaim implements Serializable {
 
     @ManyToOne(optional = false)
     @NotNull
+    @JsonIgnoreProperties("equipmentReservations")
     private EquipmentBundle equipmentBundle;
 
     @ManyToOne(optional = false)
